b33547b28666e974e2afd78c1126ae59e17d40b2
Harmanpreet Singh <hsingh71@sapient.com>
2019-08-31 11:15:12 +0530

    Vidly | Moved sort logic to the moviesTable component

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -44,15 +44,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = path => {
-    console.log(path);
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 
@@ -94,6 +86,7 @@ class Movies extends Component {
           <p>Showing {filtered.length} movies in the database.</p>
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
