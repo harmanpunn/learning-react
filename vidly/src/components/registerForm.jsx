fcefd6d7393e1304254e6a06db5af4ee5f7f6b07
Harmanpreet Singh <hsingh71@sapient.com>
2019-09-04 01:37:15 +0530

    Vidly | Added a registration form.

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -2,40 +2,44 @@ import React, { Component } from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
-class LoginForm extends Form {
+class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { username: "", password: "", name: "" },
     errors: {}
   };
 
   schema = {
     username: Joi.string()
       .required()
+      .email({ minDomainAtoms: 2 })
       .label("Username"),
     password: Joi.string()
       .required()
-      .label("Password")
+      .min(5)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
   };
-
-  //username = React.createRef();
 
   doSubmit = () => {
     //Call the server
-    console.log("Submitted");
+    console.log("User Registered");
   };
 
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("username", "Username")}
           {this.renderInput("password", "Password", "password")}
-          {this.renderButton("Login")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
         </form>
       </div>
     );
   }
 }
 
-export default LoginForm;
+export default RegisterForm;
